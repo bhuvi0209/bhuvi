5aba0ab1bdb35616a4ff6fd8b615c0d5cdd75aab
bhuvi0209 <bhuvaneswarihari2@gmail.com>
2025-07-04 20:34:58 +0530

    Update calculator.py

--- Sample Projects/calculator.py
+++ Sample Projects/calculator.py
@@ -1,1 +1,21 @@
+def calculator():
+    num1 = float(input("enter the first number (num1): "))
+    num2 = float(input("enter the second number (num2): "))
+    operator = input("enter the operator ('+', '-', '*', '/' ): ")
 
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
+
+result = calculator()
+print(result)
